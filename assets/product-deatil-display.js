f859292639d44fd8b417f778ff6dc2857deac8d7
venkateshGolla-06 <vgolla@marmeto.com>
2024-05-22 16:58:38 +0530

    all

--- assets/product-deatil-display.js
+++ assets/product-deatil-display.js
@@ -3,8 +3,9 @@ console.log(`${productUrl}?section_id=template--22732303794450__main`)
     fetch(`${productUrl}?sections=main-product`)
     .then((response) => response.json())
           .then((responseText) => {
+            console.log(responseText)
     const html = new DOMParser().parseFromString(responseText,'text/html');
-    console.log(html["main-product"])
+    console.log(html)
               const container = document.getElementById("custom-featured-product");
               console.log(container)
             const content = html.querySelector(".main-product-page")
